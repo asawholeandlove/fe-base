f2d9ed9ebe70fb6806cff2c9b704279eda84803e
dangthaii <thaidv@hivetech.vn>
2024-07-01 10:29:44 +0700

    add some libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as ReactRouter } from "react-router-dom";
+import { App as AntdApp } from "antd";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ReactRouter>
+      <QueryClientProvider client={queryClient}>
+        <AntdApp>
+          <App />
+        </AntdApp>
+      </QueryClientProvider>
+    </ReactRouter>
+  </React.StrictMode>,
 );
