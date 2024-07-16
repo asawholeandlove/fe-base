d1cecc2c4dbf6cf869c9524837ddb1a668c72813
dangthaii <thaidv@hivetech.vn>
2024-07-16 09:23:18 +0700

    update

--- src/pages/form/FormModify/components/FieldsForm.tsx
+++ src/pages/form/FormModify/components/FieldsForm.tsx
@@ -1,27 +1,67 @@
-import { Button, Form, Input, Select, Switch } from "antd";
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  CopyOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import { Button, Form, Space, Switch } from "antd";
 import EFormItem from "~/components/antdBase/EFormItem";
 import EInput from "~/components/antdBase/EInput";
 
 export default function FieldsForm() {
+  const form = Form.useFormInstance();
   return (
     <Form.List name={"fields"}>
-      {(fields, { add, remove }) => {
+      {(fields, { add, remove, move }) => {
         return (
           <>
-            {fields.map(({ key, name }) => (
-              <div key={key} className="mb-5 border border-gray-200">
-                <div className="flex items-center justify-between"></div>
+            {fields.map(({ key: k, name }) => (
+              <div key={k} className="mb-5 rounded-lg border border-gray-200">
+                <div className="mb-3 flex items-center justify-between rounded-lg bg-gray-300 p-3">
+                  <Form.Item noStyle shouldUpdate>
+                    {() => {
+                      return (
+                        <div className="text-base font-semibold">
+                          {form.getFieldValue(["fields", name, "label"])}
+                        </div>
+                      );
+                    }}
+                  </Form.Item>
+                  <Space className="cursor-pointer">
+                    <ArrowUpOutlined onClick={() => move(name, name - 1)} />
+                    <ArrowDownOutlined onClick={() => move(name, name + 1)} />
+                    <CopyOutlined
+                      onClick={() =>
+                        add(
+                          {
+                            ...form.getFieldValue(["fields", name]),
+                          },
+                          name + 1,
+                        )
+                      }
+                    />
+                    <DeleteOutlined
+                      onClick={() => remove(name)}
+                      className="text-red-600"
+                    />
+                  </Space>
+                </div>
                 <div className="m-auto max-w-[800px]">
                   <EFormItem label="Tiêu đề" required name={[name, "label"]}>
                     <EInput />
                   </EFormItem>
                   <EFormItem
                     label="Required?"
-                    required
                     name={[name, "required"]}
                     valuePropName="checked"
                   >
                     <Switch />
+                  </EFormItem>
+                  <EFormItem
+                    label="Giá trị mặc định"
+                    name={[name, "defaultValue"]}
+                  >
+                    <EInput />
                   </EFormItem>
                 </div>
               </div>
